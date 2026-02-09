fc078e8949a1dd5a3d120f58b9963aeb154f6864
St John, Austin <pokeboy7to8@gmail.com>
2026-02-08 21:06:27 -0800

    Complete

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -5,18 +5,23 @@ using namespace std;
 int main()
 {
 	// print name of program
-	cout << "Rectangle Calculator" << endl << endl;
+	cout << "Rectangle Calculator" << endl;
 
 	// get height and width from user
 	double height;
 	double width;
-	cout << "Enter height and width: ";
+	cout << "Enter height: " <<endl;
+	cin >> height;
+	cout << "Enter width: "<<endl;
+	cin >> width;
 /* write your code here */
 	// calculate area
+	double area;
+	area = height * width;
 	
 
 	// write output to console
-	
+	cout<<"The area of the rectangle is: "<<area<<endl;
 
 	// return value that indicates normal program exit
     return 0;
