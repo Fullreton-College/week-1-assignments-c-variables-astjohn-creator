f12ecb4b1c2cd39fd55eb9ce462fe746c7f27d53
St John, Austin <pokeboy7to8@gmail.com>
2026-02-08 21:15:36 -0800

    Complete

--- Assignment2_PaceConverter.cpp
+++ Assignment2_PaceConverter.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 int main()
 {
-    double mph = 6.5;
+    double mph;
+    mph = 6.5
     // PACE calculation
     /* write your code */
+    double totalMinutes = 60.0/mph;
 
     //express pace in minutes and seconds
   /* write your code here */
+  int minutes = static_cast<int>(totalMinutes);
+  double seconds;
+  seconds = (totalMinutes - minutes) * 60.0;
 
   //express your output in 1 decimal points
+  cout<< fixed << setprecision(1);
+  cout << "Pace: " << minutes << "  minutes and " << seconds << " seconds" << endl;
+
+  return 0;
 
 
 }
